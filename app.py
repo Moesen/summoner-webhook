200be9b6ce67ac7263f69c76410d131b81172eaf
Gustav Lang Moesmand <42034018+Moesen@users.noreply.github.com>
2020-09-28 12:01:00 +0200

    webpapge to show tracked stats

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template
-from core.DataCollectionStrategy.tasks import get_stats_for_all
+import json
 
-
-app = Flask(__name__, template_folder="core/DataCollectionStrategy/templates")
+app = Flask(__name__, template_folder="core/DataExportStrategy/Flask/templates", static_folder="core/DataExportStrategy/Flask/static")
 
 @app.route("/")
 def index():
-    stats = get_stats_for_all()
-    print()
-    return render_template("index.html", stats = stats)
+    summoners_stats = json.loads(open("core/DataStoringStrategy/summoners_data.json", "r").read())
+    return render_template("index.html", summoners_stats=summoners_stats)
 
 if __name__ == "__main__":
     app.debug = True
